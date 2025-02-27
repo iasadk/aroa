3ce50c32778516906c7bb4da58fe637374d6c3cd
Asad Khan <iasad0017@gmail.com>
2025-02-27 22:13:13 +0530

    Initial Project setup.

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BookmarkTab = () => {
+  return (
+    <View>
+      <Text>BookmarkTab</Text>
+    </View>
+  )
+}
+
+export default BookmarkTab
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CreateTab = () => {
+  return (
+    <View>
+      <Text>CreateTab</Text>
+    </View>
+  )
+}
+
+export default CreateTab
+
+const styles = StyleSheet.create({})
